410aedc7a76f0029ed2f315cdd36d59cedcd63ed
Kyle Lui <kylui@berkeley.edu>
2020-04-01 17:00:48 -0400

    basic window prefab, view cube start

---
+++ UI Elements - Kyle/Assets/Scripts/ViewCube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.name);
+                if (hit.transform.name == "MyObjectName")
+                { Debug.Log("My object is clicked by mouse"); }
+            }
+        }
+
+    }
+}
