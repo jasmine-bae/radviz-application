6e09b123449bae5e9e314fa4ed4a225753935033
Jasmine Bae <jasmine.bae@berkeley.edu>
2020-06-06 19:49:30 -0700

    Revamped UI

---
+++ First Iteration - RadViz/Assets/Scripts/moveCamera3D.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCamera3D : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /*
+    Writen by Windexglow 11-13-10.  Use it, edit it, steal it I don't care.  
+    Converted to C# 27-02-13 - no credit wanted.
+    Simple flycam I made, since I couldn't find any others made public.  
+    Made simple to use (drag and drop, done) for regular keyboard layout  
+    wasd : basic movement
+    shift : Makes camera accelerate
+    space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
+
+
+    float mainSpeed = 5.0f; //regular speed
+    float shiftAdd = 5.0f; //multiplied by how long shift is held.  Basically running
+    float maxShift = 20.0f; //Maximum speed when holdin gshift
+    float camSens = 1.0f; //How sensitive it with mouse
+    private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
+    private float totalRun = 1.0f;
+    private float X;
+    private float Y;
+
+    void Update()
+    {
+
+        if (Input.GetMouseButton(1))
+        {
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * camSens, -Input.GetAxis("Mouse X") * camSens, 0));
+            X = transform.rotation.eulerAngles.x;
+            Y = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(X, Y, 0);
+        }
+
+        float ScrollWheelChange = Input.GetAxis("Mouse ScrollWheel");           //This little peece of code is written by JelleWho https://github.com/jellewie
+        if (ScrollWheelChange != 0)
+        {                                            //If the scrollwheel has changed
+            float R = ScrollWheelChange * 15;                                   //The radius from current camera
+            float PosX = transform.eulerAngles.x + 90;              //Get up and down
+            float PosY = -1 * (transform.eulerAngles.y - 90);       //Get left to right
+            PosX = PosX / 180 * Mathf.PI;                                       //Convert from degrees to radians
+            PosY = PosY / 180 * Mathf.PI;                                       //^
+            float X = R * Mathf.Sin(PosX) * Mathf.Cos(PosY);                    //Calculate new coords
+            float Z = R * Mathf.Sin(PosX) * Mathf.Sin(PosY);                    //^
+            float Y = R * Mathf.Cos(PosX);                                      //^
+            float CamX = transform.position.x;                      //Get current camera postition for the offset
+            float CamY = transform.position.y;                      //^
+            float CamZ = transform.position.z;                      //^
+            transform.position = new Vector3(CamX + X, CamY + Y, CamZ + Z);//Move the main camera
+        }
+
+        //Keyboard commands
+        Vector3 p = GetBaseInput();
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            totalRun += Time.deltaTime;
+            p = p * totalRun * shiftAdd;
+            p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
+            p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
+            p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+        }
+        else
+        {
+            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+            p = p * mainSpeed;
+        }
+
+        p = p * Time.deltaTime;
+        Vector3 newPosition = transform.position;
+        if (Input.GetKey(KeyCode.Space))
+        { //If player wants to move on X and Z axis only
+            transform.Translate(p);
+            newPosition.x = transform.position.x;
+            newPosition.z = transform.position.z;
+            transform.position = newPosition;
+        }
+        else
+        {
+            transform.Translate(p);
+        }
+
+    }
+
+    private Vector3 GetBaseInput()
+    { //returns the basic values, if it's 0 than it's not active.
+        Vector3 p_Velocity = new Vector3();
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            p_Velocity += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            p_Velocity += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            p_Velocity += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            p_Velocity += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            p_Velocity += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            p_Velocity += new Vector3(0, -1, 0);
+        }
+        return p_Velocity;
+    }
+}
